d9cc436f7cd96a527db3fc2839b67d2452ab5233
r0han99 <r409067@gmail.com>
2020-07-23 23:29:44 +0530

    created a duplicate segement with synchronised version of the pytouch for future development and feature introduction
    made few changes in the script, touch header.log is now sudoer

---
+++ Linux+Mac/ForDev/help.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+print('\n\n')
+
+# Manual
+
+print('                       M A N U A L             ')
+
+print('\n')
+
+print('\033[1m' + 'NAME' + '\033[0m'+'\n        --creates a python file with preset imports for the project')
+
+print('\n\n')
+
+print('\033[1m' + 'SYNOPSIS' + '\033[0m' + '\n' + '         '+'\033[1m'+'pytouch'+'\033[0m'+' --[option]'+' [filename.py]')
+
+print('\n')
+
+print('\033[1m' + 'DESCRIPTION' + '\033[0m' +'\n' + '         '+'\033[0m'+'The Pytouch utility creates an python file with a header of import statements currently required for the project files or while during a daily practise session.The import statements are to be preset in the logs using options integrated in the utility.relieves you, the user, from the tire of writing the same import statements again and again.')
+
+print('\n')
+
+print('         '+'\033[1m'+'--set'+' | -s'+'\033[0m'+'                       presetting the required modules.')
+
+print('\n')
+
+print('         '+'\033[1m'+'--truncate'+' | -t'+'\033[0m'+'                  erasing the log file with preset headers.')
+
+print('\n')
+
+print('         '+'\033[1m'+'--file'+' | -f '+'\033[0m'+'[filename.py ...]     filename to create a script with the preset headers.')
+
+print('\n')
+
+print('         '+'\033[1m'+'--display'+' | -d'+'\033[0m'+'                  displays the log file with preset imports statements.')
+
+print('\n')
+
+print('         '+'\033[1m'+'--edit'+' | -e'+'\033[0m'+'                     edit the pre-existing import statements in the log.')
+
+print('\n')
+
+print('         '+'\033[1m'+'--add'+' | -a'+'\033[0m'+'                      adding more import statements to the log file.')
+
+
+print('\n')
+
+print('         '+'\033[1m'+'-m --file-chmod'+' | -m -fch'+'\033[0m'+'      Creates a file with provided filename, converts it into an Executable with an Interpreter.')
+
+
+print('\n')
+
+print('         '+'\033[1m'+'--bash'+' | -b'+'\033[0m'+'                     Creates an Executable bash script.')
+
+
+
+
+
+
+
+
+print('\n\n'+'\033[1m'+'Improvise.Adapt.Overcome')
+
+print('\n')
+
+print('                                                 20,June 2020.                                     ')
+
+print('\n')
+
+print('\033[1m'+'                                                  [E N D]                                         ')
+
+print('\033[0m')
